14a5cf3598f90094ba299862a78a9786262d515b
Emmy Manning <emswoo@EM.local>
2024-05-19 12:12:59 -0400

    router

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import HeroSection from './pages/HeroSection';
+
 const App = () => {
   return (
-    <div>App</div>
-  )
-}
+    <Routes>
+      <Route path='/' element={<HeroSection />} />
+    </Routes>
+  );
+};
 
-export default App
+export default App;
