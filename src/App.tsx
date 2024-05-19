1f1eadaa8a3d6f0603ca41ff8b67e6404290afa9
Emmy Manning <emswoo@EM.local>
2024-05-19 12:22:05 -0400

    about, gallery, contact pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,8 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import HeroSection from './pages/HeroSection';
+import About from './pages/About';
+import Gallery from './pages/Gallery';
+import Contact from './pages/Contact';
 import NavBar from './components/NavBar';
 
 const App = () => {
@@ -8,6 +11,9 @@ const App = () => {
       <NavBar />
       <Routes>
         <Route path='/' element={<HeroSection />} />
+        <Route path='about' element={<About />} />
+        <Route path='gallery' element={<Gallery />} />
+        <Route path='contact' element={<Contact />} />
       </Routes>
     </>
   );
