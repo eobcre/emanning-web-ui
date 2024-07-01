84141988d66f01d3cee3b9b744f8972b50b0cb14
Emmy Manning <emswoo@EM.local>
2024-07-01 12:53:24 -0400

    implemented github api

--- src/pages/HeroSection/Section.tsx
+++ src/pages/HeroSection/Section.tsx
@@ -1,7 +1,16 @@
-import { GoFileDirectory } from 'react-icons/go';
+import { useRetrieveRepos } from '../../hooks/useRepo';
+import RepoCard from './RepoCard';
+
+interface RepoData {
+  id: number;
+  name: string;
+}
 
 const Section = () => {
-  const divStyle = 'bg-[#333333] bg-opacity-50 rounded -mt-3 sm:-mt-4 p-20 md:-mt-5 lg:-mt-6';
+  const { data: repoData } = useRetrieveRepos();
+  console.log('repoData', repoData);
+
+  const divStyle = 'grid lg:grid-cols-2 gap-8 bg-[#333333] bg-opacity-50 rounded -mt-3 sm:-mt-4 p-20 md:-mt-5 lg:-mt-6';
   const h1Style = 'custom-text text-5xl font-bold sm:text-[8vw]';
 
   return (
@@ -9,59 +18,12 @@ const Section = () => {
       <div className='xl:px-36'>
         <div className='pt-20'>
           <h1 className={h1Style}>GitHub</h1>
-          {/* latest */}
           <div className={divStyle}>
-            <div className='grid grid-cols-1 md:grid-cols-2 gap-10'>
-              <div className='border rounded px-6 py-5'>
-                <div className='flex justify-between items-center gap-2'>
-                  <span>Chat GPT Clone</span>
-                  <GoFileDirectory size={28} />
-                </div>
-                <div className='pt-4'>
-                  <p>
-                    Lorem ipsum dolor sit amet consectetur, adipisicing elit. Asperiores enim repellat est odio ab iure rerum quas
-                    officia deserunt a harum sequi laboriosam cupiditate, earum totam, velit aut, eaque quisquam.
-                  </p>
-                </div>
+            {repoData?.map((data: RepoData) => (
+              <div key={data.id}>
+                <RepoCard data={data} />
               </div>
-              <div className='border rounded px-6 py-5'>
-                <div className='flex justify-between items-center gap-2'>
-                  <span>Chat GPT Clone</span>
-                  <GoFileDirectory size={28} />
-                </div>
-                <div className='pt-4'>
-                  <p>
-                    Lorem ipsum dolor sit amet consectetur, adipisicing elit. Asperiores enim repellat est odio ab iure rerum quas
-                    officia deserunt a harum sequi laboriosam cupiditate, earum totam, velit aut, eaque quisquam.
-                  </p>
-                </div>
-              </div>
-              <div className='border rounded px-6 py-5'>
-                <div className='flex justify-between items-center gap-2'>
-                  <span>Chat GPT Clone</span>
-                  <GoFileDirectory size={28} />
-                </div>
-                <div className='pt-4'>
-                  <p>
-                    Lorem ipsum dolor sit amet consectetur, adipisicing elit. Asperiores enim repellat est odio ab iure rerum quas
-                    officia deserunt a harum sequi laboriosam cupiditate, earum totam, velit aut, eaque quisquam.
-                  </p>
-                </div>
-              </div>
-              <div className='border rounded px-6 py-5'>
-                <div className='flex justify-between items-center gap-2'>
-                  <span>Chat GPT Clone</span>
-                  <GoFileDirectory size={28} />
-                </div>
-                <div className='pt-4'>
-                  <p>
-                    Lorem ipsum dolor sit amet consectetur, adipisicing elit. Asperiores enim repellat est odio ab iure rerum quas
-                    officia deserunt a harum sequi laboriosam cupiditate, earum totam, velit aut, eaque quisquam.
-                  </p>
-                </div>
-              </div>
-            </div>
-            {/* })} */}
+            ))}
           </div>
         </div>
       </div>
