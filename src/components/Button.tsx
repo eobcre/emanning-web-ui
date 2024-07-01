85811e0ed1b8110c283cd30db4f79e171eb76019
Emmy Manning <emswoo@EM.local>
2024-07-01 09:28:22 -0400

    new component

---
+++ src/components/Button.tsx
@@ -0,0 +1,10 @@
+type ButtonProps = {
+    buttonName: string;
+    className: string;
+  };
+  
+  const Button: React.FC<ButtonProps> = ({ buttonName, className }) => {
+    return <button className={className}>{buttonName}</button>;
+  };
+  
+  export default Button;
