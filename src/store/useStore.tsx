842bf2966b269229133e17975e977a7b630dd0e1
Emmy Manning <emswoo@EM.local>
2024-08-18 13:31:00 -0400

    dark mode

--- src/store/useStore.tsx
+++ src/store/useStore.tsx
@@ -4,12 +4,18 @@ interface StoreState {
   isModalOpen: boolean;
   openModal: () => void;
   closeModal: () => void;
+
+  isDarkMode: boolean;
+  toggleDarkMode: () => void;
 }
 
 const useStore = create<StoreState>((set) => ({
   isModalOpen: false,
   openModal: () => set({ isModalOpen: true }),
   closeModal: () => set({ isModalOpen: false }),
+
+  isDarkMode: false,
+  toggleDarkMode: () => set((state) => ({ isDarkMode: !state.isDarkMode })),
 }));
 
 export default useStore;
