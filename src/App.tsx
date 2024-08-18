0bb226e4eb91f0603e6aef72d8de976302cee4af
Emmy Manning <emswoo@EM.local>
2024-08-18 13:32:31 -0400

    removed nav route

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,10 @@ import HeroSection from './pages/HeroSection/HeroSection';
 import About from './pages/About';
 import Gallery from './pages/Gallery';
 import Contact from './pages/Contact';
-import NavBar from './components/NavBar';
 
 const App = () => {
   return (
     <>
-      <NavBar />
       <Routes>
         <Route path='/' element={<HeroSection />} />
         <Route path='/about' element={<About />} />
