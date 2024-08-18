62ec438127be7bfd4a8e1f53fd2e959efa7dfb46
Emmy Manning <emswoo@EM.local>
2024-08-18 19:50:47 -0400

    moved

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,40 @@
-import { Routes, Route } from 'react-router-dom';
-import HeroSection from './pages/HeroSection/HeroSection';
+import { useRef } from 'react';
+import Hero from './pages/Hero';
 import About from './pages/About';
 import Gallery from './pages/Gallery';
 import Contact from './pages/Contact';
+import Footer from './components/Footer';
 
 const App = () => {
+  const aboutRef = useRef<HTMLDivElement | null>(null);
+  const galleryRef = useRef<HTMLDivElement | null>(null);
+  const contactRef = useRef<HTMLDivElement | null>(null);
+
+  const scrollNextSection = () => {
+    if (aboutRef.current) {
+      aboutRef.current.scrollIntoView({ behavior: 'smooth' });
+    }
+  };
+
+  const scrollToSection = (sectionRef: React.RefObject<HTMLDivElement> | null) => {
+    if (sectionRef && sectionRef.current) {
+      sectionRef.current.scrollIntoView({ behavior: 'smooth' });
+    }
+  };
+
   return (
     <>
-      <Routes>
-        <Route path='/' element={<HeroSection />} />
-        <Route path='/about' element={<About />} />
-        <Route path='/gallery' element={<Gallery />} />
-        <Route path='/contact' element={<Contact />} />
-      </Routes>
+      <Hero scrollNextSection={scrollNextSection} scrollToSection={scrollToSection} aboutRef={aboutRef} galleryRef={galleryRef} contactRef={contactRef} />
+      <div ref={aboutRef} className='text-white bg-light-mode p-10'>
+        <About />
+      </div>
+      <div ref={galleryRef} className='text-white bg-light-mode px-10'>
+        <Gallery />
+      </div>
+      <div ref={contactRef} className='text-white bg-light-mode px-10 pt-10'>
+        <Contact />
+      </div>
+      <Footer />
     </>
   );
 };
