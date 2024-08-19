897e0df62fd2547eadf17020e29edddf32826033
Emmy Manning <emswoo@EM.local>
2024-08-18 22:01:43 -0400

    fixed nav header

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -17,7 +17,6 @@ interface NavProps {
 }
 
 const Nav: React.FC<NavProps> = ({ isNavOpen, setNavOpen, toggleMode, navData }) => {
-  const [isSwitchLabelVisible, setSwitchLabelVisible] = useState(false);
   const { isDarkMode } = useStore();
 
   const ref = useRef<HTMLDivElement | null>(null);
@@ -28,11 +27,9 @@ const Nav: React.FC<NavProps> = ({ isNavOpen, setNavOpen, toggleMode, navData })
         if (window.scrollY > 0) {
           ref.current.classList.add(isDarkMode ? 'dark-nav-bg' : 'light-nav-bg');
           ref.current.classList.remove('bg-transparent');
-          setSwitchLabelVisible(false);
         } else {
           ref.current.classList.add('bg-transparent');
           ref.current.classList.remove(isDarkMode ? 'dark-nav-bg' : 'light-nav-bg');
-          setSwitchLabelVisible(true);
         }
       }
     };
@@ -45,7 +42,7 @@ const Nav: React.FC<NavProps> = ({ isNavOpen, setNavOpen, toggleMode, navData })
   return (
     <nav
       ref={ref}
-      className={`fixed top-0 right-0 z-10 border-l-2 border-l-light-mode h-full transition-transform transform md:h-[80px] md:w-full md:border-none md:transform-none md:flex md:justify-between md:items-center md:px-2 ${
+      className={`fixed top-0 right-0 z-10 border-l-2 border-l-light-mode h-full transition-transform transform md:h-[7rem] md:w-full md:border-none md:transform-none md:flex md:justify-between md:items-center md:px-2 ${
         isNavOpen ? 'translate-x-0' : 'translate-x-full'
       } ${window.innerWidth < 768 ? (isDarkMode ? 'dark-nav-bg' : 'light-nav-bg') : 'bg-transparent'}`}
     >
@@ -65,12 +62,12 @@ const Nav: React.FC<NavProps> = ({ isNavOpen, setNavOpen, toggleMode, navData })
         ))}
       </ul>
       <div className='flex flex-col justify-center items-center mt-14 md:mt-0'>
-        {isSwitchLabelVisible && <span className={`font-bold ${isDarkMode ? 'text-dark-mode' : 'text-light-mode'}`}>{isDarkMode ? 'OFF' : 'ON'}</span>}
         <Button
           buttonName={isDarkMode ? <Icon icon='mdi:light-switch-off' /> : <Icon icon='mdi:light-switch' />}
           className={`text-[4rem] ${isDarkMode ? 'text-dark-mode' : 'text-light-mode'}`}
           onClick={toggleMode}
         />
+        <span className={`font-bold ${isDarkMode ? 'text-dark-mode' : 'text-light-mode'}`}>{isDarkMode ? 'OFF' : 'ON'}</span>
       </div>
     </nav>
   );
