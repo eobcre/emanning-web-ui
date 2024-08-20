020c346d39190e430cfa171d2719676ef8719928
Emmy Manning <emswoo@EM.local>
2024-08-20 14:38:32 -0400

    added props

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,13 +1,24 @@
-import React, { ReactNode } from 'react';
+import { ReactNode } from 'react';
 
 type ButtonProps = {
   buttonName: ReactNode;
   className?: string;
   onClick?: () => void;
+  href?: string;
+  target?: string;
+  rel?: string;
+  onMouseEnter?: () => void;
+  onMouseLeave?: () => void;
 };
 
-const Button: React.FC<ButtonProps> = ({ buttonName, className, onClick }) => {
-  return <button className={className} onClick={onClick}>{buttonName}</button>;
+const Button: React.FC<ButtonProps> = ({ buttonName, className, onClick, onMouseEnter, onMouseLeave, href, target, rel }) => {
+  return (
+    <button className={className} onClick={onClick} onMouseEnter={onMouseEnter} onMouseLeave={onMouseLeave}>
+      <a href={href} target={target} rel={rel}>
+        {buttonName}
+      </a>
+    </button>
+  );
 };
 
 export default Button;
