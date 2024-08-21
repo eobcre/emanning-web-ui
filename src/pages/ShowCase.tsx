7fbbce103c5074e5379792cf320a5881772e5b4b
Emmy Manning <emswoo@EM.local>
2024-08-20 21:15:52 -0400

    fixed responsive

--- src/pages/ShowCase.tsx
+++ src/pages/ShowCase.tsx
@@ -12,7 +12,7 @@ const ShowCase = () => {
       <header className='pl-6 sm:pl-8 pr-2 py-4'>
         <Icon icon='weui:back-filled' onClick={() => navigate('/')} className='text-5xl hover:opacity-50' />
       </header>
-      <div className='flex justify-center items-center h-[70vh] sm:py-32 sm:px-28'>
+      <div className='flex justify-center items-center h-[70vh] sm:block sm:py-32 sm:px-28'>
         <h1 className={`temporary-font text-[6rem] leading-[6rem] sm:text-[12rem] sm:leading-[12rem] uppercase ${isDarkMode ? 'text-dark-mode' : 'text-light-mode'}`}>
           Coming
           <br />
