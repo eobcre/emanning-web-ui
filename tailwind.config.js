a09c14d998457314870dc08b369f41b148665f6b
Emmy Manning <emswoo@EM.local>
2024-08-27 11:40:00 -0400

    updated screen

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,12 +3,17 @@ export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {
+      screens: {
+        mobile: { max: '640px' }, // isMobile
+        tablet: { min: '641px', max: '1024px' }, // isTablet
+        desktop: { min: '1025px', max: '1823px' }, // isDesktop
+      },
       colors: {
         'dark-mode': '#e4d5b7',
         'light-mode': '#303030',
-        'dark': '#5A5339',
-        'light': '#e2c311'
-      }
+        dark: '#5a5339',
+        light: '#e2c311',
+      },
     },
   },
   plugins: [],
