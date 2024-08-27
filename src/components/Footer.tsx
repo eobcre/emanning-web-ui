1f1a33563383dc88d41c3e6338ef1aee6671c072
Emmy Manning <emswoo@EM.local>
2024-08-27 12:15:05 -0400

    updated layout

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,19 +3,16 @@ import { getFooterIconData } from '../data/data';
 
 const Footer = () => {
   return (
-    <footer className='bg-light-mode flex justify-center text-white text-lg px-6 py-10 w-full'>
-      <div className='flex flex-col lg:flex-row'>
-        <div className='mb-1'>
-          <p className='mr-10'>&copy; {new Date().getFullYear()} Emmy Manning All Rights Reserved.</p>
-        </div>
-        <div className='lg:flex'>
-          <div className='flex gap-4'>
-            {getFooterIconData.map((icon) => (
-              <Link key={icon.id} to={icon.href} target={icon.target} rel={icon.rel}>
-                {icon.icon}
-              </Link>
-            ))}
-          </div>
+    <footer className='text-sm tablet:text-lg desktop:text-lg bg-light-mode flex flex-col tablet:flex-row desktop:flex-row justify-center items-center text-white px-6 py-10 w-full'>
+      <p className='tablet:mr-10 desktop:mr-10'>&copy; {new Date().getFullYear()} Emmy Manning All Rights Reserved.</p>
+      {/* icon */}
+      <div className='lg:flex'>
+        <div className='flex justify-center gap-4 pt-4 tablet:pt-0 desktop:pt-0'>
+          {getFooterIconData.map((icon) => (
+            <Link key={icon.id} to={icon.href} target={icon.target} rel={icon.rel}>
+              {icon.icon}
+            </Link>
+          ))}
         </div>
       </div>
     </footer>
