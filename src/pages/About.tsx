55b2aba60678804a9f1a9b7d690aa7f528f8f0ed
Emmy Manning <emswoo@EM.local>
2024-09-23 16:34:25 -0400

    updated description style

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -44,14 +44,14 @@ const About = () => {
           {/* about section */}
           <div className='flex flex-col justify-center gap-6 md:h-screen'>
             <p className={`text-2xl font-bold ${isDarkMode ? 'text-dark-mode' : 'text-light-mode'}`}>Full Stack Software Developer based in PA.</p>
-            <p className={`text-md md:text-lg ${isDarkMode ? 'text-dark-mode' : 'text-light-mode'}`}>
+            <p className={`text-md md:text-lg opacity-80 ${isDarkMode ? 'text-dark-mode' : 'text-light-mode'}`}>
               Fast-forward to the present, I am currently developing an operating system tailored for financial services, where I integrate both ends expertise to create seamless and secure
               applications. I began my journey in the industry as a software tester, where I honed my attention to detail and problem solving skills. My career path took me through diverse roles,
               including map alignment for Google Map. I've made meaningful contributions to developing medical surveys, emphasizing my commitment to projects that impact lives. I've also had the
               privilege of leading a Frontend Developer Internship at a startup specializing in web development, which allowed me to learn, develop, mentor, and guide the team. I am always passionate
               about creative coding and driven by an insatiable curiosity to learn new technologies.
             </p>
-            <p className={`text-md md:text-lg ${isDarkMode ? 'text-dark-mode' : 'text-light-mode'}`}>
+            <p className={`text-md md:text-lg opacity-80 ${isDarkMode ? 'text-dark-mode' : 'text-light-mode'}`}>
               During my time away from the computer, I'm usually reading, cooking and enjoying a coffee at cafe. These moments away from the screen recharge my creativity and keep me inspired.
             </p>
           </div>
@@ -61,7 +61,7 @@ const About = () => {
               <span className={`text-sm md:text-base ${isDarkMode ? 'text-dark-mode' : 'text-light-mode'}`}>{work.date}</span>
               <p className={`text-xl md:text-xl font-bold mt-1 ${isDarkMode ? 'text-dark-mode' : 'text-light-mode'}`}>{work.name}</p>
               <p className={`text-sm md:text-base font-bold mt-1 ${isDarkMode ? 'text-dark-mode' : 'text-light-mode'}`}>{work.position}</p>
-              <p className={`md:text-lg mt-4 ${isDarkMode ? 'text-dark-mode' : 'text-light-mode'}`}>{work.description}</p>
+              <p className={`md:text-lg opacity-80 mt-4 ${isDarkMode ? 'text-dark-mode' : 'text-light-mode'}`}>{work.description}</p>
               {/* tag */}
               <div className={`flex flex-wrap gap-4 text-sm mt-10 ${isDarkMode ? 'text-dark-mode' : 'text-light-mode'}`}>
                 {work.tech.map((tech, index) => (
