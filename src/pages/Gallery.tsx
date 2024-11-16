67d3f8c87220ae28baffa7b0fc3b8dabadcc84cb
Emmy Manning <emswoo@EM.local>
2024-11-16 12:33:25 -0500

    refactored code

--- src/pages/Gallery.tsx
+++ src/pages/Gallery.tsx
@@ -1,11 +1,9 @@
 import { useState } from 'react';
 import useStore from '../store/useStore';
 import { useRetrieveRepos } from '../hooks/useRepo';
+import { getMyProjects } from '../data/galleryData';
 import RepoCard from '../components/RepoCard';
 import Button from '../components/Button';
-import Work_Task_Image from '../assets/task_management_app.png';
-import Work_Balance_App from '../assets/balance_app.png'
-import Work_Unsplash_Image from '../assets/unsplash_screen.png';
 interface RepoData {
   id: number;
   name: string;
@@ -26,17 +24,11 @@ const Gallery = () => {
           <div className='relative'>
             <h1 className={`custom-font text-5xl uppercase mb-14 sm:text-[8vw] xs:mb-0 ${isDarkMode ? 'text-dark-mode' : 'text-light-mode'}`}>Work</h1>
             <div className='grid grid-cols-1 lg:grid-cols-3 gap-4 pb-28'>
-              <a href='https://emanning-app.xyz' target='_blank' rel='noopener noreferrer' className='hover:opacity-70'>
-                <img src={Work_Task_Image} alt='image' className='rounded-md w-full' />
-              </a>
-              <div className='relative'>
-              <a href='https:/balanceapp.io' target='_blank' rel='noopener noreferrer' className='hover:opacity-70'>
-                <img src={Work_Balance_App} alt='image' className='rounded-md w-full' />
-              </a>
-              </div>
-              <div>
-                <img src={Work_Unsplash_Image} alt='image' className='rounded-md opacity-50 w-full' />
-              </div>
+              {getMyProjects.map((project) => (
+                <a href={project.link} target='_blank' rel='noopener noreferrer' className={`${project.hoverEffect ? 'hover:opacity-70' : ''}`}>
+                  <img src={project.image} alt='image' className={project.style} />
+                </a>
+              ))}
             </div>
             <h1 className={`custom-font text-5xl uppercase mb-14 sm:text-[8vw] xs:mb-0 ${isDarkMode ? 'text-dark-mode' : 'text-light-mode'}`}>Latest Code</h1>
             {/* <div className={`absolute top-[4.5rem] border-t-[20px] border-t-[#303030] opacity-50 w-full`}></div> */}
