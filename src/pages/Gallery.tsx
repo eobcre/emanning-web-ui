71d34a0c60fae505ac3e54025f26d1ccd7acbdd8
Emmy Manning <emswoo@EM.local>
2024-11-16 11:25:00 -0500

    updated work

--- src/pages/Gallery.tsx
+++ src/pages/Gallery.tsx
@@ -4,6 +4,7 @@ import { useRetrieveRepos } from '../hooks/useRepo';
 import RepoCard from '../components/RepoCard';
 import Button from '../components/Button';
 import Work_Task_Image from '../assets/image.png';
+import Work_Balance_App from '../assets/balance_app.png'
 import Work_Unsplash_Image from '../assets/unsplash_screen.png';
 interface RepoData {
   id: number;
@@ -28,6 +29,11 @@ const Gallery = () => {
               <a href='https://emanning-app.xyz' target='_blank' rel='noopener noreferrer' className='hover:opacity-70'>
                 <img src={Work_Task_Image} alt='image' className='rounded-md w-[320px]' />
               </a>
+              <div className='relative'>
+              <a href='https:/balanceapp.io' target='_blank' rel='noopener noreferrer' className='hover:opacity-70'>
+                <img src={Work_Balance_App} alt='image' className='rounded-md w-[436px]' />
+              </a>
+              </div>
               <div className='relative'>
                 <img src={Work_Unsplash_Image} alt='image' className='rounded-md opacity-50 w-[440px] h-[255px]' />
                 <p className='absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-xl text-dark font-bold uppercase'>Coming Soon...</p>
