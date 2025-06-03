143f8d6c45ad972a8798533ffddf193b07829090
Emmy Manning <emswoo@EM.local>
2025-06-02 21:28:58 -0400

    removed screens

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,11 +3,6 @@ export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {
-      screens: {
-        mobile: { max: '640px' }, // isMobile
-        tablet: { min: '641px', max: '1024px' }, // isTablet
-        desktop: { min: '1025px', max: '1823px' }, // isDesktop
-      },
       colors: {
         'dark-mode': '#e4d5b7',
         'light-mode': '#303030',
