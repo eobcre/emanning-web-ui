5c3bac6f6a5c239b5ef09868258e2d357632c380
Emmy Manning <emswoo@EM.local>
2025-06-09 20:28:58 -0400

    new layout

--- src/pages/Hero.tsx
+++ src/pages/Hero.tsx
@@ -5,23 +5,18 @@ import { getNavData } from '../data/data';
 import useStore from '../store/useStore';
 import Nav from '../components/Nav';
 import Button from '../components/Button';
-import { FaChevronDown } from 'react-icons/fa6';
 
 interface MainProps {
-  scrollNextSection: () => void;
   scrollToSection: (section: React.RefObject<HTMLDivElement> | null) => void;
   aboutRef: React.RefObject<HTMLDivElement>;
   galleryRef: React.RefObject<HTMLDivElement>;
   contactRef: React.RefObject<HTMLDivElement>;
 }
 
-const Hero: React.FC<MainProps> = ({ scrollNextSection, scrollToSection, aboutRef, galleryRef, contactRef }) => {
+const Hero: React.FC<MainProps> = ({ scrollToSection, aboutRef, galleryRef, contactRef }) => {
   const [isNavOpen, setNavOpen] = useState(false);
-  const [isNameVisible, setNameVisible] = useState(false);
   const [isTitleVisible, setTitleVisible] = useState(false);
-  const [isSubTitleVisible, setSubTitleVisible] = useState(false);
-  const [isButtonVisible, setButtonVisible] = useState(false);
-  const [isArrowVisible, setArrowVisible] = useState(false);
+  const [isNextTitleVisible, setNextTitleVisible] = useState(false);
   const [isHoverCV, setHoverCV] = useState(false);
   const { isDarkMode, toggleDarkMode } = useStore();
 
@@ -34,32 +29,17 @@ const Hero: React.FC<MainProps> = ({ scrollNextSection, scrollToSection, aboutRe
   const navData = getNavData(scrollToTop, scrollToSection, aboutRef, galleryRef, contactRef);
 
   useEffect(() => {
-    const delayName = setTimeout(() => {
-      setNameVisible(true);
-    }, 50);
-
     const delayTitle = setTimeout(() => {
       setTitleVisible(true);
     }, 500);
 
-    const delaySubTitle = setTimeout(() => {
-      setSubTitleVisible(true);
+    const delayNextTitle = setTimeout(() => {
+      setNextTitleVisible(true);
     }, 1500);
 
-    const delayButton = setTimeout(() => {
-      setButtonVisible(true);
-    }, 3000);
-
-    const delayArrow = setTimeout(() => {
-      setArrowVisible(true);
-    }, 4000);
-
     return () => {
-      clearTimeout(delayName);
       clearTimeout(delayTitle);
-      clearTimeout(delaySubTitle);
-      clearTimeout(delayButton);
-      clearTimeout(delayArrow);
+      clearTimeout(delayNextTitle);
     };
   }, []);
 
@@ -73,6 +53,7 @@ const Hero: React.FC<MainProps> = ({ scrollNextSection, scrollToSection, aboutRe
 
   return (
     <div className={`flex flex-col md:py-4 w-full min-h-screen ${isDarkMode ? 'dark-mode' : 'light-mode'}`}>
+      {/* nav */}
       <div className='md:block'>
         <div className='flex justify-end p-4 md:hidden'>
           <Button
@@ -88,48 +69,20 @@ const Hero: React.FC<MainProps> = ({ scrollNextSection, scrollToSection, aboutRe
         </div>
         <Nav isNavOpen={isNavOpen} setNavOpen={setNavOpen} toggleMode={toggleMode} navData={navData} />
       </div>
-      <div className='flex flex-col justify-center items-center min-h-[16rem] md:block grow sm:mt-28 px-10'>
-        {isNameVisible && <span className={`text-xl font-bold uppercase pb-[-40px] ${isDarkMode ? 'text-dark-mode' : 'text-light-mode'} ${isNameVisible ? 'block-fade-in' : ''}`}>Emmy Manning</span>}
-        {isTitleVisible && (
-          <h1
-            className={`custom-font text-[8rem] leading-[8rem] mt-4 sm:text-[14rem] sm:leading-[14rem] uppercase ${isDarkMode ? 'text-dark-mode' : 'text-light-mode'} ${
-              isTitleVisible ? 'block-fade-in' : ''
-            }`}
-          >
-            Craft
-            <br />
-            And Dev
-          </h1>
-        )}
-        <div className='min-h-[3rem]'>
-          {isSubTitleVisible && (
-            <p className={`text-xl font-bold uppercase py-3 ${isDarkMode ? 'text-dark-mode' : 'text-light-mode'} ${isSubTitleVisible ? 'block-fade-in' : ''}`}>Full Stack Developer</p>
-          )}
+      {/* contents */}
+      <div className='flex flex-col justify-center min-h-screen px-10'>
+        <div className='leading-none'>
+          <span className={`custom-500-font text-[0.8rem] sm:text-[1.1rem] uppercase ${isDarkMode ? 'text-dark-mode' : 'text-light-mode'}`}>Emmy Manning | Full Stack Developer</span>
+          <h1 className={`custom-600-font text-[4.6rem] sm:text-[14vw] tracking-tighter ${isDarkMode ? 'text-olive' : 'text-yellow'} ${isTitleVisible ? 'fade-in' : ''}`}>iCREATE</h1>
+          <h1 className={`custom-500-font text-[5.2rem] sm:text-[14vw] tracking-tighter ${isDarkMode ? 'text-dark-mode' : 'text-light-mode'} ${isNextTitleVisible ? 'fade-in' : ''}`}>eNJOY</h1>
         </div>
-        <div className='min-h-[6rem]'>
-          {isButtonVisible && (
-            <div className={`flex gap-7 ${isButtonVisible ? 'block-fade-in' : ''}`}>
-              <Button
-                buttonName={isHoverCV ? 'View' : 'CV'}
-                onClick={() => navigate('./emanning-cv')}
-                className={`${
-                  isDarkMode ? 'bg-dark-mode text-light-mode uppercase font-bold rounded-md mt-6 p-3 w-[10rem]' : 'bg-light-mode text-white uppercase font-bold rounded-md mt-6 p-3 w-[10rem]'
-                }`}
-                onMouseEnter={() => setHoverCV(true)}
-                onMouseLeave={() => setHoverCV(false)}
-              />
-            </div>
-          )}
-        </div>
-      </div>
-      <div className='min-h-[3rem]'>
-        {isArrowVisible && (
-          <div className='flex justify-center mb-4'>
-            <div onClick={scrollNextSection}>
-              <FaChevronDown size={30} className={`cursor-pointer hover:opacity-70 ${isDarkMode ? 'text-dark-mode' : 'text-light-mode'}`} />
-            </div>
-          </div>
-        )}
+        <Button
+          buttonName={isHoverCV ? <Icon icon='material-symbols:play-arrow' width='24' height='24' /> : <span className='custom-500-font tracking-widest'>CV</span>}
+          onClick={() => navigate('./emanning-cv')}
+          className={`${isDarkMode ? 'bg-dark-mode text-light-mode rounded-full mt-6 p-4 w-[3.5rem]' : 'bg-light-mode text-white rounded-full mt-6 p-4 w-[3.5rem]'}`}
+          onMouseEnter={() => setHoverCV(true)}
+          onMouseLeave={() => setHoverCV(false)}
+        />
       </div>
     </div>
   );
